84eaa0ae2dd5c9cc1dc91441a86b8c5d413be240
Casinnam <Casinnam@users.noreply.github.com>
2026-05-22 21:40:07 -0700

    Use canonical www domain for auth

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.EVERYTHING_CONVERT_SUPABASE = {
   url: 'https://tuwhuftbjqkgduukvbfv.supabase.co',
   anonKey: 'sb_publishable_Y6tx3YNPVh56QruGfVkEnw_gfissksf',
-  publicOrigin: 'https://everythingconvert.com/',
+  publicOrigin: 'https://www.everythingconvert.com/',
 };
